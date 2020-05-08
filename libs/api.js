82d08df66c0df54ced119f9a1a2b594d6e3fcebf
Petr Reshetin <preshetin@gmail.com>
2020-05-08 14:07:27 +0300

    chore: Remove comments

--- libs/api.js
+++ libs/api.js
@@ -34,24 +34,6 @@ export const getRecentJobsFromDesignedSite = async () => {
     }
   })
 
-//  const jobs = [
-//    {
-//      id: 1,
-//      title: "Kiosk Design for Shopping Centre (Pretzels)",
-//      description: "Love Pretzels | Anywhere",
-//      location: "Anywhere",
-//      url: "https://www.behance.net/joblist/72091/Kiosk-Design-for-Shopping-Centre-(Pretzels)?utm_source=designed&utm_medium=organic", 
-//    },
-//    {
-//      id: 2,
-//      title: "UI/UX Designer",
-//      description: "AltraDimension Technologies Private Linited | Freelance",
-//      location: "Anywhere",
-//      image_url: "https://mir-s3-cdn-cf.behance.net/team/100/ec1dcc257315.5eac422711974.jpg",
-//      url: "https://www.behance.net/joblist/72705/UIUX-Designer?utm_source=designed&utm_medium=organic"
-//    },
-//  ];
-
   console.log(response.data.data.getAllJobs[0])
 
   const jobs = response.data.data.getAllJobs
